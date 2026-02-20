04a6519f85e9083f12d03ba0e4e269b89cea6540
Camila Richter <ric23183@uvg.edu.gt>
2026-02-19 18:55:52 -0600

    ejercicio 2.2

--- lab03.js
+++ lab03.js
@@ -59,3 +59,108 @@
     }
   }
 ]
+
+//ejercicio 2.2
+[
+  {
+    '$lookup': {
+      'from': 'transactions', 
+      'localField': 'accounts', 
+      'foreignField': 'account_id', 
+      'as': 'transactions_info'
+    }
+  }, {
+    '$unwind': {
+      'path': '$transactions_info'
+    }
+  }, {
+    '$unwind': {
+      'path': '$transactions_info.transactions'
+    }
+  }, {
+    '$group': {
+      '_id': {
+        'customer_id': '$_id', 
+        'name': '$name'
+      }, 
+      'total_buys': {
+        '$sum': {
+          '$cond': [
+            {
+              '$eq': [
+                '$transactions_info.transactions.transaction_code', 'buy'
+              ]
+            }, {
+              '$toDouble': '$transactions_info.transactions.total'
+            }, 0
+          ]
+        }
+      }, 
+      'total_sells': {
+        '$sum': {
+          '$cond': [
+            {
+              '$eq': [
+                '$transactions_info.transactions.transaction_code', 'sell'
+              ]
+            }, {
+              '$toDouble': '$transactions_info.transactions.total'
+            }, 0
+          ]
+        }
+      }
+    }
+  }, {
+    '$project': {
+      '_id': 0, 
+      'name': '$_id.name', 
+      'balance': {
+        '$subtract': [
+          '$total_sells', '$total_buys'
+        ]
+      }
+    }
+  }, {
+    '$project': {
+      'name': 1, 
+      'balance': 1, 
+      'category': {
+        '$switch': {
+          'branches': [
+            {
+              'case': {
+                '$lt': [
+                  '$balance', 5000
+                ]
+              }, 
+              'then': 'Bajo'
+            }, {
+              'case': {
+                '$and': [
+                  {
+                    '$gte': [
+                      '$balance', 5000
+                    ]
+                  }, {
+                    '$lte': [
+                      '$balance', 20000
+                    ]
+                  }
+                ]
+              }, 
+              'then': 'Medio'
+            }, {
+              'case': {
+                '$gt': [
+                  '$balance', 20000
+                ]
+              }, 
+              'then': 'Alto'
+            }
+          ], 
+          'default': 'Sin categoría'
+        }
+      }
+    }
+  }
+]
